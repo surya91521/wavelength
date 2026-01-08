432e6bb34d68ed97faa5cd4530f3e1b1965b2b58
surya91521 <suryanarayanan.s@browserstack.com>
2026-01-08 21:48:55 +0530

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/wavelength/",
+  base: mode === "development" ? "/" : "./",
   server: {
     host: "::",
     port: 7070,
